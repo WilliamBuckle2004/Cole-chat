d92ef5867b9634d06efc7c6879637c12fbe4866f
SnoozeGruntSilver420 <lesilversaiyan@gmail.com>
2017-07-13 16:39:59 -0500

    memes

--- Jeff.js
+++ Jeff.js
@@ -1,2 +1,28 @@
-//     alert ("Hi and welcome to the earthbound chat with ");
-//  var chatbox = document.querySelector('div#chatbox');
+var box = document.querySelector("div#chatbox");
+
+chatbox.insertAdjacentHTML('afterbegin',`<p class='royalblue'> Welcome too Ninjago plus! </p>`);
+//------------------------------------------------------------------------------------------
+
+var button = document.querySelector('button')
+var inputBox = document.querySelector('input');
+
+button.addEventListener( 'click', function() {
+    
+    box.insertAdjacentHTML("beforeend", "<p>" + inputBox.value + "</p>")
+} )
+
+//---------------------------------------------------------------------------------------------
+mascotFeedback();
+function mascotFeedback(){
+
+    setTimeout(function () {
+    box.insertAdjacentHTML("beforeend", "<p> Welcome to the team! </p>");
+}, 500);
+
+}
+
+var mascot = {
+    Name: "Cole",
+
+}
+box.scrollTo(0, box.scrollHieght)
